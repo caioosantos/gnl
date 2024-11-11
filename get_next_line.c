b7a5c06f17deb00f20051d48b6701a58fd2936ab
Caio Henrique Brito Santos <cbrito-s@c3r7p2.42sp.org.br>
2024-11-11 19:59:14 -0300

    gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,39 @@
 /*   By: cbrito-s <cbrito-s>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:50:36 by cbrito-s          #+#    #+#             */
-/*   Updated: 2024/11/11 19:18:21 by cbrito-s         ###   ########.fr       */
+/*   Updated: 2024/11/11 19:58:29 by cbrito-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_line(int fd, char *buf)
+char	*get_line(int fd, char *buf)
 {
+	char	*line;
+	char	*next_line;
+	char	*temp_line;
+	ssize_t	read_bytes;
+
+	read_bytes = read(fd, line, BUFFER_SIZE);
+	temp_line = line;
+	while (read_bytes > 0)
+	{
+		if (ft_strchr(line, '\n'))
+		{
+			next_line = (ft_strchr(line, '\n'));
+			return(line);
+		}
+		else
+		{
+			read_bytes = read(fd, line, BUFFER_SIZE);
+			ft_strjoin(temp_line, line);
+		}
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	int			read_bytes;
 	static char	*buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
@@ -26,6 +46,6 @@ char	*get_next_line(int fd)
 	buffer = ft_calloc(1, sizeof(char));
 	if (!buffer)
 		return (NULL);
-	read_bytes = get_line(fd, buffer);
+	buffer = get_line(fd, buffer);
 	return (0);
 }
